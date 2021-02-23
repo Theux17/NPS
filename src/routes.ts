7917e3fddb841f84e7692a2e96470165ed40c50f
Theux17 <oliveira.ms18@gmail.com>
2021-02-23 12:29:36 -0300

    Creating user in the database

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express'
+import { UserController } from './controllers/UserControllers'
 
 const routes = Router()
+
+const userController = new UserController
+
+routes.post("/users", userController.create)
 
 export default routes
