835fc1bd03978f59bc2e14b8b5283d391e3ee034
Theux17 <oliveira.ms18@gmail.com>
2021-02-24 11:16:01 -0300

    Creating surveys

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 import { UserController } from './controllers/UserControllers'
+import { SurveyController } from './controllers/SurveysController'
 
 const routes = Router()
 
 const userController = new UserController
+const surveysController = new SurveyController
 
 routes.post("/users", userController.create)
+
+routes.get("/surveys", surveysController.show)
+routes.post("/surveys", surveysController.create)
 
 export default routes
